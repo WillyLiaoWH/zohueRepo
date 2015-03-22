6728ba2b65d7c18d530a854285b40229e1662c05
ParkerPo <b00705048@ntu.edu.tw>
2015-03-22 23:31:58 +0800

    pull

--- assets/js/js_article/mainJS.js
+++ assets/js/js_article/mainJS.js
@@ -326,6 +326,8 @@ function sendEmail(){
 }
 
 function shareFB(){
-  u=location.href;t=document.title;window.open('http://www.facebook.com/sharer.php?u='+encodeURIComponent(u)+'&t='+encodeURIComponent(t),'sharer','toolbar=0,status=0,width=626,height=436');
+  u=location.href;
+  t=document.title;
+  window.open('http://www.facebook.com/sharer.php?u='+encodeURIComponent(u)+'&t='+encodeURIComponent(t),'sharer','toolbar=0,status=0,width=626,height=436');
   return false;
 }
