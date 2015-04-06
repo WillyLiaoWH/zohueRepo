cbe4c58f6849ccc6800bcb631817f4d2878d6738
ParkerPo <b00705048@ntu.edu.tw>
2015-04-06 15:56:56 +0800

    change

--- assets/js/js_public/topControl.js
+++ assets/js/js_public/topControl.js
@@ -12,9 +12,8 @@ $(window).load(function(){ // 暫存回覆頁面
 });
 
 $(document).ready(function(){  
-  FB_API();
-  checkAuth();
 
+  checkAuth();
 
   $( "#setUp" ).click(function() {
     if(setUpMenu.style.display=="block"){
@@ -45,23 +44,7 @@ $(document).ready(function(){
 
 });
 
-function FB_API(){
- window.fbAsyncInit = function() {
-    FB.init({
-      appId      : '1639694986252116',
-      xfbml      : true,
-      version    : 'v2.2'
-    });
-  };
 
-  (function(d, s, id){
-     var js, fjs = d.getElementsByTagName(s)[0];
-     if (d.getElementById(id)) {return;}
-     js = d.createElement(s); js.id = id;
-     js.src = "//connect.facebook.net/en_US/sdk.js";
-     fjs.parentNode.insertBefore(js, fjs);
-   }(document, 'script', 'facebook-jssdk'));
-}
 
 
   // This is called with the results from from FB.getLoginStatus().
