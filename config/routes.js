ae9d8d67a953479183616376b4722a0970233195
damncan <danny199281627@hotmail.com>
2015-12-07 01:10:15 +0800

    profile ejs

--- config/routes.js
+++ config/routes.js
@@ -148,25 +148,37 @@ module.exports.routes = {
   'GET /getSuspendReason': 'Backend.getSuspendReason',
   'GET /countForum': 'Articles.countForum',
   'GET /updateLastForumTime': 'User.updateLastForumTime',
+
+
+  // 'GET /profile': {
+  //   view: 'profile/index',
+  //   locals: {
+  //     scripts: [
+  //       '/js/js_public/modalBox.js-master/modalBox-min.js',
+  //       '/js/js_public/alertify.js',
+  //       '/js/js_profile/mainJS.js',
+  //       '/js/js_post/cropper.min.js',
+  //       '/js/js_profile/crop-avatar.js?ver=1'
+  //     ],
+  //     stylesheets: [
+  //       '/styles/css_profile/style.css',
+  //       '/styles/css_post/crop-avatar.css',
+  //       '/styles/css_post/cropper.min.css',
+  //       '/styles/importer.css',
+  //       '/styles/css_public/themes/alertify.core.css',
+  //       '/styles/css_public/themes/alertify.default.css'
+  //     ]
+  //   }
+  // },
+  'GET /profile/:id': {
+    controller: 'TimelinesController',
+    action: 'setProfilePage',
+    skipAssets: true
+  },
   'GET /profile': {
-    view: 'profile/index',
-    locals: {
-      scripts: [
-        '/js/js_public/modalBox.js-master/modalBox-min.js',
-        '/js/js_public/alertify.js',
-        '/js/js_profile/mainJS.js',
-        '/js/js_post/cropper.min.js',
-        '/js/js_profile/crop-avatar.js?ver=1'
-      ],
-      stylesheets: [
-        '/styles/css_profile/style.css',
-        '/styles/css_post/crop-avatar.css',
-        '/styles/css_post/cropper.min.css',
-        '/styles/importer.css',
-        '/styles/css_public/themes/alertify.core.css',
-        '/styles/css_public/themes/alertify.default.css'
-      ]
-    }
+    controller: 'TimelinesController',
+    action: 'setProfilePage',
+    skipAssets: true
   },
 
   'GET /editArticle/*': {
