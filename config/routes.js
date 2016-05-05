a1703bfe33b386fec339d327a5d81ba59ca2ea5f
jeffweilee <jeffweilee@gmail.com>
2016-05-05 15:06:11 +0800

    gettingStarted controller for mobile detection

--- config/routes.js
+++ config/routes.js
@@ -320,15 +320,9 @@ module.exports.routes = {
     }
   },
   '/gettingStarted':{
-    view:'gettingStarted/index',
-    locals:{
-      scripts: [
-      ],
-      stylesheets:[
-        '/styles/css_gettingStarted/style.css',
-        '/styles/importer.css'
-      ]
-    }
+    controller: 'GettingStartedController',
+    action: 'gettingStartedPage',
+    skipAssets: true,
   },
 
   '/': '/home'
