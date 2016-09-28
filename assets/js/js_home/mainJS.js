4c1926cdd1114e09119b1b8d01583ca64c807d4c
peterpanchien <peterpan1011288@gmail.com>
2016-09-28 20:09:26 +0800

    the photos in the home page would enlarge after clicking

--- assets/js/js_home/mainJS.js
+++ assets/js/js_home/mainJS.js
@@ -27,6 +27,43 @@ app.controller('announcementCtrl', function($scope, $http) {
 
 
 
+$(document).ready(function(){
+  
+  $(document).on("click","#img_1",function(e){
+    $("#myModal").css( "display", "block" );
+
+    // modal.style.display = "block";
+  });
+});
+
+
+
+
+// Get the modal
+// var modal = document.getElementById('myModal');
+
+// // Get the image and insert it inside the modal - use its "alt" text as a caption
+// var img = document.getElementById('img_1');
+// var modalImg = document.getElementById("img_1");
+//var captionText = document.getElementById("caption");
+// img.onclick = function(){
+//     modal.style.display = "block";
+//     modalImg.src = this.src;
+//     console.log("hello");
+// }
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+// span.onclick = function() { 
+//   modal.style.display = "none";
+// }
+
+
+
+
+
 // function homepagePic(){
 //   // console.log("in homepic");
 //     $.get("/getHomepagePic", function(homepagePics){
