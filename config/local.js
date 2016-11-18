cf519073cc8e3e269beb481e0791fda1bb709b4d
chester11206 <chester11206@gmail.com>
2016-11-18 10:16:49 +0800

    11/18 改訂閱電子報

--- config/local.js
+++ config/local.js
@@ -61,7 +61,7 @@ module.exports = {
    ***************************************************************************/
 
   // port : 80,
-  port:80,
+  port:8000,
   //port:8000,
   //   ssl:  {
   //     key: fs.readFileSync('ssl/key.pem'),
